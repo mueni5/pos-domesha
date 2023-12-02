aa5971bb91dcf1e31b803fed6732cccfbc7cfc92
+ <+>
2023-12-02 12:31:02 +0300

    cashier section

---
+++ src/Components/Cashier.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import SalesPage from './SalesPage';
+
+const Cashier = () => {
+  // Add any specific cashier-related state or logic if needed
+
+  return (
+    <div>
+      <h2>Cashier Section</h2>
+      <SalesPage />
+    </div>
+  );
+};
+
+export default Cashier;
